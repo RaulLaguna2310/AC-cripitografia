5e944f2322545df8d921a954bd74aec08e7437eb
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-18 10:08:48 -0300

    cep

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +7,49 @@ import { Observable } from 'rxjs';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http : HttpClient) {}
+  cep: string = '';
+  endereco: any = {
+    logradouro: '',
+    bairro: '',
+    localidade: '',
+    uf: ''
+  };
 
-  cep: string = ''
-
-  consultarEndereco(){
-    return this.http.get(`viacep.com.br/ws/${this.cep}/json/`)
+  consultarEndereco() {
+    if (this.cep !== '') {
+      this.http.get(`https://viacep.com.br/ws/${this.cep}/json/`).subscribe(
+        (dados) => {
+          this.endereco = dados;
+        },
+        (erro) => {
+          console.error('Erro ao consultar CEP:', erro);
+          alert('Erro ao consultar CEP. Verifique se o CEP está correto.');
+        }
+      );
+    } else {
+      alert('Por favor, insira um CEP válido.');
+    }
   }
 
+  procurarPorEndereco() {
+    const { logradouro, localidade, uf } = this.endereco;
+
+    const url = `https://viacep.com.br/ws/${logradouro}&cidade=${localidade}&estado=${uf}/json/`;
+
+    this.http.get(url).subscribe(
+      (dados: any) => {
+        if (dados && dados.length > 0) {
+          this.cep = dados[0].cep; // Ajuste conforme a estrutura da resposta
+        } else {
+          alert('Endereço não encontrado.');
+        }
+      },
+      (erro) => {
+        console.error('Erro ao procurar pelo endereço:', erro);
+        alert('Erro ao procurar pelo endereço. Verifique os dados fornecidos.');
+      }
+    );
+  }
 }
